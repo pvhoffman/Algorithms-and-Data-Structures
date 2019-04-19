75927559805fb79d92d2f9d1ac65f9ef679cc77b
Paul Hoffman <pvhoffman@protonmail.com>
2019-04-18 19:17:14 -0700

    added basic disjoint set (set_union) data structure

---
+++ Data Structures/Disjoint Set/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "disjoint-set.h"
+
+int
+main(int argc, char** argv)
+{
+    pvh::disjoint_set<int> ds(100);
+    return 0;
+}
+
+
